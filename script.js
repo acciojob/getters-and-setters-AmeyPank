cd8f9873f4081e3298bd6d324360e27fec186ebd
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-04-11 14:58:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-class Person {
-  constructor(name, age) {
-    this.name1 = name;
-    this.age1 = age;
-  }
+// class Person {
+//   constructor(name, age) {
+//     this.name1 = name;
+//     this.age1 = age;
+//   }
 
-  get name() {
-    return this.name1;
-  }
+//   get name() {
+//     return this.name1;
+//   }
 
-  set age(age) {
-    this.age = age1;
-  }
-}
+//   set age(age) {
+//     this.age = age1;
+//   }
+// }
 
-class Student extends Person {
-  study() {
-    console.log(`${this.name1} is studying`);
-  }
-}
+// class Student extends Person {
+//   study() {
+//     console.log(`${this.name1} is studying`);
+//   }
+// }
 
-class Teacher extends Person {
-  teach() {
-    console.log(`${this.name1} is teaching`);
-  }
-}
+// class Teacher extends Person {
+//   teach() {
+//     console.log(`${this.name1} is teaching`);
+//   }
+// }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+// // Do not change the code below this line
+// window.Person = Person;
+// window.Student = Student;
+// window.Teacher = Teacher;
 
 
 
